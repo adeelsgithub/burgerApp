99fe5540dba9325f9dd7a2df25b3094b208c6ef2
Muhammad Adeel <moadeel321@gmail.com>
2020-01-10 21:26:48 +0500

    using navlink

--- src/Components/NavigationItems.js
+++ src/Components/NavigationItems.js
@@ -1,12 +1,12 @@
 import React from "react";
 import CLasses from "./navigationItems.css";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 
 let navigationItems = (props) =>{
     return (
         <li className={CLasses.navigation}>
-            <Link to={props.link}> {props.item} </Link>  
+            <NavLink to={props.link}> {props.item} </NavLink>  
         </li>
 
         
